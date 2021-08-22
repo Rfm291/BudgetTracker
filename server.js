34196e9ba225fb838a816e138ab1de2352d7c9b1
Richard Mercado <RichardFM291@gmail.com>
2021-08-22 18:33:58 -0400

    server and package mods

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,16 +16,19 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Budget',
+  process.env.MONGODB_URI || 'mongodb://localhost/BudgetDB',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-);
+  })
+.then(()=>{
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+})
+.catch(console.log); 
+
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
